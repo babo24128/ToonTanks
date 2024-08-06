3ae6f5630bfb1ee72958fe25caa0af350c413dfd
babo24128 <babo24128@naver.com>
2024-08-06 18:42:19 +0900

    로컬 회전

--- Source/ToonTanks/Tank.h
+++ Source/ToonTanks/Tank.h
@@ -29,7 +29,10 @@ private:
 
 	UPROPERTY(EditAnywhere, Category = "Movement")
 	float Speed = 200.f;
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float TurnRate = 45.f;
 
 	void Move(float Value);
+	void Turn(float Value);
 		
 };
