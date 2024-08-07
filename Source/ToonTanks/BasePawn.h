42da7b6924cf39df238bd19acd8fe83fb9bc05a4
babo24128 <babo24128@naver.com>
2024-08-07 21:31:53 +0900

    마우스 커서 사용하기

--- Source/ToonTanks/BasePawn.h
+++ Source/ToonTanks/BasePawn.h
@@ -37,8 +37,4 @@ private:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Super Duper Variables", meta = (AllowPrivateAccess = "true"))
 	int32 EditAnywhereInt = 22;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
 };
